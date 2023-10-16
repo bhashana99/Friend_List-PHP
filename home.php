52320eb86e19fe62d9492bf22c04bb3fdcf0091e
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 19:16:40 +0530

    display num of friend and username

--- home.php
+++ home.php
@@ -8,3 +8,7 @@ $username = $_SESSION['username'];
 
 ?>
 
+<div class="container">
+<h2 class="mt-5 text-center"><span class="text-warning"><?php echo $username; ?></span> 's Add Friend Page</h2>
+<h3 class="mt-2 text-center"> Total Number of Friends is <span class="text-success"><?php echo $numOfFriend; ?></span> </h3>
+</div>
