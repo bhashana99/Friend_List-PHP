8f77ca4148ab770f97ff082bce7cf8d4f3dcbc95
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 09:24:58 +0530

    design home page

--- index.php
+++ index.php
@@ -3,13 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Friends</title>
+    <title>Home | My Friends</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <div class="bg-primary py-3">
         <h1 >&nbsp;My Friend System</h1>
         <link rel="stylesheet" href="style.css">
+    </div>
+
+        <div class="card text-center mt-5 mx-auto" style="width: 40vw;" >
+            <div class="card-body ">
+                <h5 class="card-title">Practical Exam :: Home Page</h5>
+                <h6 class="card-subtitle mb-2 text-muted mt-3">Name: Bhashana </h6>
+                <h6 class="card-subtitle mb-2 text-muted mt-2">Student ID: SE/2020/058 </h6>
+                <p class="card-text text-danger fw-bold shadow my-3">I declare that this project is my individual work <br> I have not worked collaboratively noe have 
+             i copied from any other student's work. </p>
+                <a href="#" class="btn btn-success">Register</a>
+                <a href="#" class="btn btn-success">Login</a>
+            </div>
+    </div>
     </div>
     
 
