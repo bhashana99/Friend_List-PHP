76bc0665e999469af1d136ec40132e0b23409432
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 19:25:39 +0530

    add button home page

--- home.php
+++ home.php
@@ -8,7 +8,13 @@ $username = $_SESSION['username'];
 
 ?>
 
-<div class="container">
+<div class="container col-6">
 <h2 class="mt-5 text-center"><span class="text-warning"><?php echo $username; ?></span> 's Add Friend Page</h2>
 <h3 class="mt-2 text-center"> Total Number of Friends is <span class="text-success"><?php echo $numOfFriend; ?></span> </h3>
+
+<div>
+<a href="#" class="btn btn-success">Add Friend</a>
+<a href="#" class="btn btn-danger">Logout</a>
+</div>
+
 </div>
