81539eff6c61bc54684ddf219ea3afe65188ebed
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 19:30:56 +0530

    link to logout and add friend page

--- home.php
+++ home.php
@@ -12,9 +12,9 @@ $username = $_SESSION['username'];
 <h2 class="mt-5 text-center"><span class="text-warning"><?php echo $username; ?></span> 's Add Friend Page</h2>
 <h3 class="mt-2 text-center"> Total Number of Friends is <span class="text-success"><?php echo $numOfFriend; ?></span> </h3>
 
-<div>
-<a href="#" class="btn btn-success">Add Friend</a>
-<a href="#" class="btn btn-danger">Logout</a>
+<div class="text-center mt-3">
+<a href="./addFriend.php" class="btn btn-success">Add Friend</a>
+<a href="./logout.php" class="btn btn-danger">Logout</a>
 </div>
 
 </div>
