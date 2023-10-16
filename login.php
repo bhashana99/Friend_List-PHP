55d566bc68a66039eb0c30da5af97ffce2f422f9
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 09:38:01 +0530

    page title

---
+++ login.php
@@ -0,0 +1,5 @@
+<?php require_once './header.php'; ?>
+
+<div class="container">
+    <h2 class="mt-5">Login Page</h2>
+</div>
--- register.php
+++ register.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register | My Friends</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php require_once './header.php'; ?>
+
+<div class="container">
+    <h2 class="mt-5 text-center text-success">Registration </h2>
+</div>
