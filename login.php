34add27d66a23be945c6f006fb365292e8187b3e
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 17:29:13 +0530

    designing login page

--- login.php
+++ login.php
@@ -1,5 +1,36 @@
-<?php require_once './header.php'; ?>
+<?php
+ require_once './header.php'; 
+ 
+ ?>
+
 
 <div class="container">
-    <h2 class="mt-5">Login Page</h2>
+    <h2 class="mt-5 text-center text-success">Sign in </h2>
+    <div class="col-md-4 mt-5 mx-auto" class="text-center">    
+        <div id="form-error">
+            <p class="text-danger"><?php  ?></p>
+        </div>
+        <form method="post" action="./action.php">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com" required  >
+            </div>
+
+            
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password"  required >
+            </div>
+            
+            <div class="d-flex justify-content-end mb-3">
+                <a href="./register.php" style="text-decoration: none;">I haven't an account.</a>
+            </div>
+
+            <div class="mb-3 text-center ">
+               <button class="btn btn-success col-12 mb-2" name="login" >Login</button>
+                 <a href="./index.php" class=" text-secondary" >Back to home page</a>
+            </div>
+        </form>
+    </div>
 </div>
