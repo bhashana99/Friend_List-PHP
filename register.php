c1d3b7e9d74cfcccc5604366b5134d872aed5e3a
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 11:27:58 +0530

    add register all field to required

--- register.php
+++ register.php
@@ -8,22 +8,22 @@
         <form method="post" action="./action.php">
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com" >
+                <input type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com" required >
             </div>
 
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" name="username" placeholder="profile name" >
+                <input type="text" class="form-control" id="username" name="username" placeholder="profile name" required >
             </div>
 
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password"  >
+                <input type="password" class="form-control" id="password" name="password"  required>
             </div>
             
             <div class="mb-3">
                 <label for="cpassword" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="cpassword" name="cpassword" >
+                <input type="password" class="form-control" id="cpassword" name="cpassword" required>
             </div>
 
             <div class="d-flex justify-content-end mb-3">
