e134c5ee916f8dba5996e6d8d45eb66989323cca
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 11:25:47 +0530

    create action file

---
+++ db-query.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once './config.php';
+
+class User extends Database{
+
+    
+}
+
+
+
+?>
--- register.php
+++ register.php
@@ -5,7 +5,7 @@
 <div class="container">
     <h2 class="mt-5 text-center text-success">Registration </h2>
     <div class="col-md-4 mt-5 mx-auto" class="text-center">
-        <form >
+        <form method="post" action="./action.php">
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com" >
