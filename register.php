e45f4e94e9ee4ce7251c59b14d6c3319ea5f163f
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 09:51:31 +0530

    password field

--- register.php
+++ register.php
@@ -14,6 +14,10 @@
                 <input type="text" class="form-control" id="username" name="username" placeholder="profile name" >
             </div>
 
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="" >
+            </div>
         </form>
     </div>
 </div>
