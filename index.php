d3ad04309114fb82c7076451b3c3e673b7334c14
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-16 09:32:07 +0530

    link register and login button

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | My Friends</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
+
    <?php require_once './header.php'; ?>
 
         <div class="card text-center mt-5 mx-auto" style="width: 40vw;" >
@@ -16,8 +8,8 @@
                 <h6 class="card-subtitle mb-2 text-muted mt-2">Student ID: SE/2020/058 </h6>
                 <p class="card-text text-danger fw-bold shadow my-3">I declare that this project is my individual work <br> I have not worked collaboratively noe have 
              i copied from any other student's work. </p>
-                <a href="#" class="btn btn-success">Register</a>
-                <a href="#" class="btn btn-success">Login</a>
+                <a href="./register.php" class="btn btn-success">Register</a>
+                <a href="./login.php" class="btn btn-success">Login</a>
             </div>
     </div>
     </div>
@@ -25,6 +17,5 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
